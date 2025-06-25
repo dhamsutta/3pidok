c657798e64fbea135070e02fd839ec5d4f9ae322
dhamsutta <dhamsutta@gmail.com>
2025-06-25 19:21:11 +0700

    Create mp3.js

---
+++ assets/js/mp3.js
@@ -0,0 +1,12 @@
+[
+  {
+    "title": "ธรรมะบรรยายชุด 1",
+    "description": "สอนเรื่องความเมตตาและปัญญา",
+    "mp3": "https://archive.org/download/buddha-wong-01/1.mp3"
+  },
+  {
+    "title": "ธรรมะบรรยายชุด 2",
+    "description": "สอนเรื่องการปล่อยวาง",
+    "mp3": "https://archive.org/download/buddha-wong-01/10.mp3"
+  }
+]
